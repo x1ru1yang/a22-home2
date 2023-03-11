2137d6c82f147657c80ff64ce058defabf52cd42
Xirui Yang <xirui.yang@student.kuleuven.be>
2023-03-11 21:14:03 +0100

    Glide lib

--- app/src/main/java/be/kuleuven/a22_home2/MainActivity.java
+++ app/src/main/java/be/kuleuven/a22_home2/MainActivity.java
@@ -1,86 +1,40 @@
 package be.kuleuven.a22_home2;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.os.Handler;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
-import be.kuleuven.a22_home2.R;
-
-public class MainActivity extends Activity {
-
-    private static final String URL = "http://192.168.4.1";
-    private Button openMailboxButton;
-    private Button closeMailboxButton;
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private final String ESP32_PATH ="http://192.168.4.2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        openMailboxButton = (Button) findViewById(R.id.openMailboxButton);
-        closeMailboxButton = (Button) findViewById(R.id.closeMailboxButton);
+        imageView = findViewById(R.id.imageView);
 
-        openMailboxButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendHttpRequest("/open_mailbox");
-            }
-        });
+        Glide.with(this)
+                .load(ESP32_PATH)
+                .into(imageView);
 
-        closeMailboxButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendHttpRequest("/close_mailbox");
-            }
-        });
-    }
 
-    private void sendHttpRequest(final String path) {
-        new Thread(new Runnable() {
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                try {
-                    URL url = new URL(URL + path);
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.connect();
-
-                    int responseCode = connection.getResponseCode();
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                        final String response = bufferedReader.readLine();
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(MainActivity.this, "Error: " + responseCode, Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "Error: Could not connect to server", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
+                Glide.with(MainActivity.this)
+                        .load(ESP32_PATH)
+                        .into(imageView);
+                handler.postDelayed(this, 50); // adjust the delay time as necessary
             }
-        }).start();
+        };
+        handler.post(runnable);
+
+
     }
 }
