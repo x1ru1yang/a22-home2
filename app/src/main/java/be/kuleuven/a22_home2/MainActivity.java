ddd23e559a124122074c0d7fa015de297f0db5bf
Xirui Yang <xirui.yang@student.kuleuven.be>
2023-03-13 21:12:59 +0100

    camera can work successfully

--- app/src/main/java/be/kuleuven/a22_home2/MainActivity.java
+++ app/src/main/java/be/kuleuven/a22_home2/MainActivity.java
@@ -1,12 +1,17 @@
 package be.kuleuven.a22_home2;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Picture;
 import android.media.Image;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
@@ -16,12 +21,13 @@ import com.bumptech.glide.Glide;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private WebView webView;
-    private final String ESP32_PATH ="http://192.168.4.2";
+    private final String ESP32_PATH ="http://192.168.4.2:443";
 
 
     @SuppressLint("WrongThread")
@@ -32,23 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = (ImageView) findViewById(R.id.imageView);
         webView = (WebView) findViewById(R.id.webview);
+
+        // enable JavaScript and other settings
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        // load the ESP32 web page
         webView.loadUrl(ESP32_PATH);
-
-        // Wait for the website to finish loading
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                // When the website finishes loading, capture a screenshot of the WebView
-                Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(bitmap);
-                view.draw(canvas);
-
-                // Now you can use the bitmap as an image stream to display the website
-                // For example, you can display it in an ImageView
-                ImageView imageView = findViewById(R.id.imageView);
-                imageView.setImageBitmap(bitmap);
-            }
-        });
-
     }
 }
