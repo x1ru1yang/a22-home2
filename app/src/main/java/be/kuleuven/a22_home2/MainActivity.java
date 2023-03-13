79f5ccb4dbe6afffae1867d8ded6fca62dfc4fdd
Xirui Yang <xirui.yang@student.kuleuven.be>
2023-03-13 20:15:19 +0100

    WebView to show a website on the APP

--- app/src/main/java/be/kuleuven/a22_home2/MainActivity.java
+++ app/src/main/java/be/kuleuven/a22_home2/MainActivity.java
@@ -1,40 +1,54 @@
 package be.kuleuven.a22_home2;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Picture;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
+    private WebView webView;
     private final String ESP32_PATH ="http://192.168.4.2";
 
+
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl(ESP32_PATH);
 
-        Glide.with(this)
-                .load(ESP32_PATH)
-                .into(imageView);
-
-
-        Handler handler = new Handler();
-        Runnable runnable = new Runnable() {
+        // Wait for the website to finish loading
+        webView.setWebViewClient(new WebViewClient() {
             @Override
-            public void run() {
-                Glide.with(MainActivity.this)
-                        .load(ESP32_PATH)
-                        .into(imageView);
-                handler.postDelayed(this, 50); // adjust the delay time as necessary
-            }
-        };
-        handler.post(runnable);
+            public void onPageFinished(WebView view, String url) {
+                // When the website finishes loading, capture a screenshot of the WebView
+                Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                view.draw(canvas);
 
+                // Now you can use the bitmap as an image stream to display the website
+                // For example, you can display it in an ImageView
+                ImageView imageView = findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
 
     }
 }
